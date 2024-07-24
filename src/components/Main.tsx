c2edf2c62d158e137722c3d997b5e8e5df65e8ec
Olli Saukko <ollisaukko@Ollis-Air.fritz.box>
2024-07-24 23:32:46 +0300

    basic template

---
+++ src/components/Main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Main: React.FC = () => {
+  return (
+    <main>
+      <h2>About Me</h2>
+      <p>
+        Hello! I'm Your Name, a web developer with a passion for creating beautiful and functional websites.
+      </p>
+      <h2>Projects</h2>
+      <ul>
+        <li>Project 1: Description</li>
+        <li>Project 2: Description</li>
+        <li>Project 3: Description</li>
+      </ul>
+    </main>
+  );
+}
+
+export default Main;
